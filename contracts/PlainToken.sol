18359ac5a7409682ce57d22590aeb8375a878ce3
Chan-Ho Suh <chanhosuh@gmail.com>
2020-04-26 14:55:55 -0400

    clear out comments

--- contracts/PlainToken.sol
+++ contracts/PlainToken.sol
@@ -11,14 +11,5 @@ contract PlainToken is ERC20("Plain Token", "PLN"), Ownable {
     constructor() public {
         _setupDecimals(DECIMALS);
         _mint(msg.sender, INITIAL_SUPPLY);
-
     }
-
-
-    // function totalSupply() public view override returns (uint256)
-    // function balanceOf(address account) public view override returns (uint256)
-    // function transfer(address recipient, uint256 amount) public virtual override returns (bool)
-    // function allowance(address owner, address spender) public view virtual override returns (uint256)
-    // function approve(address spender, uint256 amount) public virtual override returns (bool)
-    // function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool)
 }
