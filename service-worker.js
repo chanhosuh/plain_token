db321156ce758c968c4eadb69344e238069a741f
Chan-Ho Suh <chanhosuh@gmail.com>
2020-04-28 19:42:10 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/plain_token/precache-manifest.62ae8b3e7e008ae3b16381be920d76d1.js"
+  "/plain_token/precache-manifest.d81bcdc8f657ae13076a64734f6cbdb0.js"
 );
 
 self.addEventListener('message', (event) => {
