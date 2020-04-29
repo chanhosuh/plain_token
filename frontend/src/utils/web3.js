4b8421ae9cb14af9e36d4dcb393edf4634e38b34
Chan-Ho Suh <chanhosuh@gmail.com>
2020-04-28 23:25:03 -0400

    take care of metamask deprecated warning

--- frontend/src/utils/web3.js
+++ frontend/src/utils/web3.js
@@ -4,6 +4,10 @@ const initWeb3 = async () => {
   let web3;
 
   if (window.ethereum) {
+    // soon to be deprecated / removed, as reloading is bad
+    // see https://medium.com/metamask/no-longer-reloading-pages-on-network-change-fbf041942b44
+    window.ethereum.autoRefreshOnNetworkChange = false;
+
     // modern Dapp browser or using Metamask
     web3 = new Web3(window.ethereum);
     console.log("Web3 version", web3.version);
@@ -13,10 +17,6 @@ const initWeb3 = async () => {
     } catch (err) {
       console.error("User denied account access.");
     }
-
-    // soon to be deprecated / removed, as reloading is bad
-    // see https://medium.com/metamask/no-longer-reloading-pages-on-network-change-fbf041942b44
-    window.ethereum.autoRefreshOnNetworkChange = false;
   } else if (window.web3) {
     // legacy-style
     web3 = new Web3(window.web3.currentProvider);
